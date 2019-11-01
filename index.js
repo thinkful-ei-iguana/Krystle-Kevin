d799086ce72b175182698725dfed8d01e6b24e4f
Kevin Moore <moore.kevin.p@gmail.com>
2019-11-01 14:52:27 -0400

    final dog app!

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 'use strict';
 
-function getDogRequest(query, displayCallback) {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogRequest(numberOfDogs, displayDogs) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${numberOfDogs}`)
     .then(response => response.json())
-    .then(responseJson =>
-      displayDogs(responseJson))
-      .then(responseJson => displayCallback(responseJson))
+    .then(responseJson => {
+      return responseJson;})
+    .then(responseJson => displayDogs(responseJson))
     .catch(error => alert('Something went wrong.  Please try again later.'));
 }
 
 function displayDogs(responseJson) {
+  let placeholder = "";
+  responseJson.message.forEach(element => {
+    placeholder += `<img src="${element}"></img></br>`;
+  });
+  $('.dog-pics').html(placeholder);
   console.log(responseJson);
-  $('.dog-pics').html(`<img src="${responseJson.message}">`);
+}
+
+function dogSearch(data) {
+  const searchedDogs = data.message.map((item, index) => displayDogs(item));
+  $('.dog-pics').html(searchedDogs);
 }
 
 function howManyDogs() {
   $('form').on('submit', event => {
     event.preventDefault();
     const numberOfDogs = $('input').val();
-    const defaultValue = $('inpit').val();
-      $('input').val("3")
+    $('input').val('3');
     console.log(numberOfDogs);
-    console.log(defaultValue);
     $('input').val('');
-    getDogRequest();
+    getDogRequest(numberOfDogs, displayDogs);
   });
 }
-
-
-
 
 function handleDogs() {
   howManyDogs();
